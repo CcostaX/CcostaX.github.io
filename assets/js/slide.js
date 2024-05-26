d201708326d0a62871d392dcb11e11abf26d1316
CcostaX <93092772+CcostaX@users.noreply.github.com>
2024-05-26 18:42:41 +0100

    changes 4

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,26 +1,30 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+showSlides(slideIndex, "mySlides_ARUAV");
+showSlides(slideIndex, "mySlides_WEB");
+showSlides(slideIndex, "mySlides_DIMENSIONS");
+showSlides(slideIndex, "mySlides_NUTRITION");
+showSlides(slideIndex, "mySlides_DEFENDYOURCASTLEAR");
+showSlides(slideIndex, "mySlides_SIMPLELAND");
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, name) {
+  slideIndex += n;
+  showSlides(slideIndex, "mySlides_" + name);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(n, name) {
+  slideIndex = n;
+  showSlides(slideIndex, "mySlides_" + name);
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+function showSlides(n, slideClass) {
+  let slides = document.getElementsByClassName(slideClass);
+  let dots = document.getElementsByClassName("dot-" + slideClass);
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
+
+  Array.from(slides).forEach(slide => slide.style.display = "none");
+  Array.from(dots).forEach(dot => dot.className = dot.className.replace(" active", ""));
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
